6be28719dd6b8ca4ffe520680ba5ebd543afae62
OpeOginni <brightoginni123@gmail.com>
2026-03-17 13:03:55 +0100

    chore: removed installation file changes

--- packages/opencode/src/installation/index.ts
+++ packages/opencode/src/installation/index.ts
@@ -87,7 +87,7 @@ export namespace Installation {
   }
 
   export function isLocal() {
-    return CHANNEL === "local" || (VERSION.startsWith("0.0.0-") && !VERSION.startsWith("0.0.0-beta")) // Prevents all other versions exepct beta from being installed
+    return CHANNEL === "local"
   }
 
   export async function method() {
@@ -162,7 +162,7 @@ export namespace Installation {
   }
 
   export async function upgrade(method: Method, target: string) {
-    let result: Awaited<ReturnType<typeof upgradeCurl> | Process.Result> | undefined
+    let result: Awaited<ReturnType<typeof upgradeCurl>> | undefined
     switch (method) {
       case "curl":
         result = await upgradeCurl(target)
